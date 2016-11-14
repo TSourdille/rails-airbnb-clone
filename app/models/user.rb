04eea14bb3321e8df0cefa325862f5536f7b6466
Julian Honma <julian.honma@gmail.com>
2016-11-14 19:37:53 +0100

    add booked_boats association to user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   has_many :boats
-  has_many :bookings, through: :boats
-
+  has_many :bookings
+  has_many :booked_boats, through: :boats, source: :bookings
   validates :first_name, :last_name, presence: true
-
-
 end
