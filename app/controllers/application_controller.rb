b3ba2c84abe709cb924471cd992bf4e3bac97aaa
Theophile Sourdille <theophile.sourdille@yahoo.fr>
2016-11-14 17:30:22 +0100

    fix signup issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
