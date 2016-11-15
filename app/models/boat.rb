232891010a3a706fbb333321a8b734fb285201b0
Julian Honma <julian.honma@gmail.com>
2016-11-15 12:33:06 +0100

    restrict boat_types to 6 choices.
    Add boat types array to boat model, add validation, modify form to
    display list of boat types as a dropdown menu.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,8 +1,19 @@
 class Boat < ApplicationRecord
+  BOAT_TYPES = [
+    "Bateau à moteur",
+    "Semi-rigide",
+    "Zodiac",
+    "Voilier",
+    "Multicoque",
+    "Péniche"
+  ]
+  CITIES = []
+
   belongs_to :user
   has_many :bookings
   has_many :availabilities
   # has_one :user, through: :booking
 
   validates :boat_type, :name, :city, :capacity, :day_rate, :user_id, presence: true
+  validates :boat_type, inclusion: { in: BOAT_TYPES }
 end
