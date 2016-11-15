653e0e1256cc728e8c04e4f06960ed992bc7796d
Florian Bataille <bataille.florian@gmail.com>
2016-11-15 12:58:52 +0100

    add index, show, boatsview - add routes boat - views pas jolies

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,9 +1,9 @@
 class BoatsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @boats = Boat.all
   end
-
 
   def show
     @boat = Boat.find(params[:id])
