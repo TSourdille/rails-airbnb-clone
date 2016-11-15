f14710da931b79932d1b8d07f88bb21aff48e6fa
Julian Honma <julian.honma@gmail.com>
2016-11-15 12:16:41 +0100

    implement boats new+create method and boats#new view/form.

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,4 +1,31 @@
 class BoatsController < ApplicationController
   def new
+    @user = current_user
+    @boat = @user.boats.new
+  end
+  def create
+    user = current_user
+    boat = user.boats.new(boat_params)
+    if boat.save
+      redirect_to root_path # TODO: boat
+      # TODO: flash success
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def boat_params
+    params.require(:boat).permit(
+    :boat_type,
+    :name,
+    :city,
+    :capacity,
+    :description,
+    :specs,
+    :equipment,
+    :day_rate
+    )
   end
 end
