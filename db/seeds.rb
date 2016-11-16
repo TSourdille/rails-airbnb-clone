d1236ea545df7eb41c77cf10750dd638aa5b082d
Julian Honma <julian.honma@gmail.com>
2016-11-16 11:29:30 +0100

    modify seed to hydrate new fields in boats, clean up seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
 MODELS = [User, Boat, Availability, Booking]
 
-def prompt_user(model, action)
-  info = "#{model.count} #{model.name.downcase}s in database - "
-  action == "delete all" ? action = action.upcase.light_red : action = action.upcase.light_green
-  prompt = " ? [y/n] "
-  print info + action + prompt
-  answer = STDIN.gets.chomp
-  answer == "y"
+def delete_seed
+  typeset_title("Deleting seed", "light_red")
+  models_to_destroy = MODELS.reverse.map { |m| prompt_user(m, "delete all")}
+  delete(models_to_destroy)
+end
+
+def create_seed
+  typeset_title("Creating seed", "light_green")
+  models_to_create = MODELS.map { |m| prompt_user(m, "create new")}
+  seed(models_to_create)
 end
 
 def delete(boolean_array)
@@ -28,11 +31,7 @@ def seed(boolean_array)
       case index
       when 0
         if boolean
-          typeset_title("Seeding users", "light_green")
-          typeset("How many users would you like to create?\n".light_cyan)
-          print "> "
-          number = STDIN.gets.chomp.to_i
-          seed_users(number)
+          seed_users(prompt_for_user_number)
         end
       when 1
         if boolean
@@ -40,30 +39,19 @@ def seed(boolean_array)
           if User.count < 1
             typeset("You need to create users before creating boats !\n".light_magenta)
           else
-            ignore = "db/samboat_urls_all_boats.json"
-            json_files = Dir["db/*.json"].reject { |f| f == ignore }
-            index = -1
-            while index < 0 || index > json_files.length
-              typeset("Which JSON file woud you like to seed from?\n".light_cyan)
-              json_files.each_with_index { |f, i| puts "#{i.to_s.red} - #{f}" }
-              print "> "
-              index = STDIN.gets.chomp.to_i
-            end
-            json_file = json_files[index]
+            json_file = prompt_for_json_file
             seed_boats(json_file)
           end
         end
       when 2
         if boolean
           typeset_title("Seeding availabilities", "light_green")
-          typeset("Feature hasn't been coded yet :(\n".light_magenta)
-          sleep(0.5)
+          inform_not_available
         end
       when 3
         if boolean
           typeset_title("Seeding bookings", "light_green")
-          typeset("Feature hasn't been coded yet :(\n".light_magenta)
-          sleep(0.5)
+          inform_not_available
         end
       end
     end
@@ -112,9 +100,14 @@ def seed_boats(json_filepath)
 
       new_boat.update(
       boat_type: boat["type"],
+      length: boat["length"],
       name: boat["name"],
       city: boat["city"],
       capacity: boat["capacity"],
+      beds: boat["beds"],
+      image1: boat["images"][0],
+      image2: boat["images"][1],
+      image3: boat["images"][2],
       description: boat["description"],
       specs: specs,
       equipment: equipment,
@@ -127,7 +120,6 @@ def seed_boats(json_filepath)
   sleep(2)
 end
 
-# Doesn't work
 def seed_bookings
   Boat.all.each do |boat|
     rand(1..3).times do
@@ -146,8 +138,42 @@ def seed_bookings
   end
 end
 
+def prompt_user(model, action)
+  info = "#{model.count} #{model.name.downcase}s in database - "
+  action == "delete all" ? action = action.upcase.light_red : action = action.upcase.light_green
+  prompt = " ? [y/n] "
+  print info + action + prompt
+  answer = STDIN.gets.chomp
+  answer == "y"
+end
+
+def prompt_for_user_number
+  typeset_title("Seeding users", "light_green")
+  typeset("How many users would you like to create?\n".light_cyan)
+  print "> "
+  STDIN.gets.chomp.to_i
+end
+
+def prompt_for_json_file
+  ignore = "db/samboat_urls_all_boats.json"
+  json_files = Dir["db/*.json"].reject { |f| f == ignore }
+  index = -1
+  while index < 0 || index > json_files.length
+    typeset("Which JSON file woud you like to seed from?\n".light_cyan)
+    json_files.each_with_index { |f, i| puts "#{i.to_s.red} - #{f}" }
+    print "> "
+    index = STDIN.gets.chomp.to_i
+  end
+  json_files[index]
+end
+
+def inform_not_available
+  typeset("Feature hasn't been coded yet :(\n".light_magenta)
+  sleep(0.5)
+end
+
 def typeset(string)
-  string.each_char { |chr| print chr ; sleep(0.03) }
+  string.each_char { |chr| print chr ; sleep(0.027) }
 end
 
 def typeset_title(string, color)
@@ -157,26 +183,22 @@ def typeset_title(string, color)
   end
 end
 
-typeset("\nWelcome to JooLeeAnh's Express Seeding services !\n".light_yellow)
-
-typeset_title("Deleting seed", "light_red")
-models_to_destroy = MODELS.reverse.map { |m| prompt_user(m, "delete all")}
-delete(models_to_destroy)
-
-typeset_title("Creating seed", "light_green")
-models_to_create = MODELS.map { |m| prompt_user(m, "create new")}
-seed(models_to_create)
-
-typeset("\nSeeding done :)\n".light_green)
-
-typeset("\nAre you happy ? [y/n] ".light_cyan)
-answer = STDIN.gets.chomp
-if answer == "y"
-  puts ""
-  typeset("Thank you for using JooLeeAnh's Express Seeding services !\n\n".light_yellow)
-  sleep(2)
-else
-  sleep(1)
-  typeset("\nFuck you\n\n".red)
-  sleep(0.2)
+def get_feedback
+  typeset("\nAre you happy ? [y/n] ".light_cyan)
+  answer = STDIN.gets.chomp
+  if answer == "y"
+    puts ""
+    typeset("Thank you for using JooLeeAnh's Express Seeding services !\n\n".light_yellow)
+    sleep(2)
+  else
+    sleep(1)
+    typeset("\nFuck you\n\n".red)
+    sleep(0.2)
+  end
 end
+
+typeset("\nWelcome to JooLeeAnh's Express Seeding services !\n".light_yellow)
+delete_seed
+create_seed
+typeset("\nSeeding done :)\n".light_green)
+get_feedback
