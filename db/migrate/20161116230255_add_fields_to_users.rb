65086f0e6aec327f693d57bcf17d433cfa599dda
Julian Honma <julian.honma@gmail.com>
2016-11-17 10:59:38 +0100

    remove phone field from users.

--- db/migrate/20161116230255_add_fields_to_users.rb
+++ db/migrate/20161116230255_add_fields_to_users.rb
@@ -2,7 +2,6 @@ class AddFieldsToUsers < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :country, :string
     add_column :users, :registered_at, :date
-    add_column :users, :phone, :string
     add_column :users, :picture_url, :string
     add_column :users, :avatar_url, :string
   end
