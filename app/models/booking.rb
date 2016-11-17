816ad4ccb5ad9d717fcf45ae1fdfff0320b1289c
Julian Honma <julian.honma@gmail.com>
2016-11-17 15:37:09 +0100

    attempt to date validation.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,19 @@
 class Booking < ApplicationRecord
   belongs_to :boat
   belongs_to :user
-  # belongs_to :user, through: :boat
+  validate :booking_period_not_overlapped
 
-  validates :start_at, :end_at, presence: true
+  validates :start_at, :boat_id, :user_id, :end_at, presence: true
+
+  private
+
+  def booking_period_not_overlapped
+    unless Booking.where(
+      '(start_at <= ? AND end_at >= ?) OR (start_at >= ? AND start_at <= ?)',
+      start_at, start_at,
+      start_at, end_at
+      ).empty?
+      errors.add(:start_at, 'Invalid period.')
+    end
+  end
 end
