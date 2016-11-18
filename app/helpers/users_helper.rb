49fbcaae6f7bab547f98b4e1e8f12039b6c26ef7
Florian Bataille <bataille.florian@gmail.com>
2016-11-18 15:58:32 +0100

    show style - add bookings at user_show

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+  def bookings_by_time(bookings)
+    past_bookings = bookings.select { |b| b.end_at < Date.today }
+    future_bookings = bookings.select { |b| b.start_at > Date.today }
+    present_bookings = bookings.select do |b|
+      b.start_at <= Date.today && b.end_at >= Date.today || b.start_at >= Date.today && b.start_at <= Date.today
+    end
+    {
+      past: past_bookings,
+      future: future_bookings,
+      present: present_bookings
+    }
+  end
 end
